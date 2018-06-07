9dd3dfcc61ff95357721707a58f8925ea26da820
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-06-07 12:35:25 +0200

    Revamp routing

--- app.js
+++ app.js
@@ -6,18 +6,8 @@ const exphbs = require('express-handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({defaultLayout: 'home'}));
 app.set('view engine', 'handlebars');
-app.get('/', (req, res)=>{
-    res.render('home/index');
-})
-app.get('/about', (req, res)=>{
-    res.render('home/about');
-})
-app.get('/login', (req, res)=>{
-    res.render('home/login');
-})
-app.get('/register', (req, res)=>{
-    res.render('home/register');
-})
+const main = require('./routes/home/main');
+app.use('/', main);
 app.listen(4500, ()=> {
     console.log("Listening live on 4500");
 })
